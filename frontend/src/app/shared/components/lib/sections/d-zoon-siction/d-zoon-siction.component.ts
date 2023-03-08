7f9bf7d5b8cfdebdf58cbfb782910bd852ccb672
Ahmed Aoulad Amar <>
2023-03-08 10:21:15 +0100

    sv

--- frontend/src/app/shared/components/lib/sections/d-zoon-siction/d-zoon-siction.component.ts
+++ frontend/src/app/shared/components/lib/sections/d-zoon-siction/d-zoon-siction.component.ts
@@ -8,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DZoonSictionComponent {
   // col = [20, 80];
-  panelSizes: number[] = [20, 80];
   minSizes: number[] = [10, 10];
   row: any[] = [20, [20, [20, 80]]];
-  col: any[] = [20, [80, [20, 80]]];
+  col: any[] = [30, [30, [20, 80]]];
+
+  panelSizes: any = [
+    { row: 20, col: [20, [20, [20, 80]]] },
+    { row: 40, col: [80, 20] },
+    { row: 20, col: [20, [20, [20, 80]]] },
+    { row: [20, [20, 80]], col: [20, [20, [20, 80]]] },
+  ];
 
   ngOnInit() {}
 
